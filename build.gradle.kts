d2ade7ea5c1ee5b8ddfccccf5a3ff206290b1197
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-03-04 17:59:47 +0900

    feat: JPA, mysql, lombok 관련 Dependency 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("java")
+    // 🌱 이미 만든 프로젝트에 spring 적용 🌱
+    id("org.springframework.boot") version "3.3.1"
+    id("io.spring.dependency-management") version "1.1.5"
 }
 
 group = "org.fastcampus"
@@ -10,8 +13,29 @@ repositories {
 }
 
 dependencies {
+    // 🌱 이미 만든 프로젝트에 spring 적용 🌱
+    // 🌱 Spring Data JPA 추가 🌱
+    // spring
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    // API 기능 구현 후 파라미터로 들어왔을 때 에러 처리
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // mysql
+    // mysql 연동을 위한 connector 라이브러리
+    runtimeOnly("com.mysql:mysql-connector-java")
+
+    // lombok
+    implementation("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    // test
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    //
 }
 
 tasks.test {
